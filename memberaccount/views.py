ef33653b08358b87bb89b3fa069930c87ca0e4ec
deklirk <abedjuma@gmail.com>
2019-03-22 22:04:42 +0300

    Modified templates to display data from database.

--- memberaccount/views.py
+++ memberaccount/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Member_share
 
-def member_list(request):
-    return render(request, 'memberaccount/member_list.html', {})
+def member_list(request):	
+	members = Member_share.objects.filter(date_posted__lte=timezone.now()).order_by('date_posted')
+	return render(request, 'memberaccount/member_list.html', {'members':members})
