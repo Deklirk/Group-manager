5e6406725b643ec9b44ff6fe6bf7611315e06bfc
deklirk <abedjuma@gmail.com>
2019-03-24 19:44:15 +0300

    Pages for the main Web pages are being created.

--- groupmanager/settings.py
+++ groupmanager/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'memberaccount',
+    'memberaccount.apps.MemberaccountConfig',
+    'webpages.apps.WebpagesConfig',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +56,9 @@ ROOT_URLCONF = 'groupmanager.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,3 +123,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# Redirect to home URL after login (Default redirects to /accounts/profile/)
+LOGIN_REDIRECT_URL = '/'
--- groupmanager/urls.py
+++ groupmanager/urls.py
@@ -18,5 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('memberaccount.urls')),
+    path('', include('webpages.urls')),
+    path('memberaccount/', include('memberaccount.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
